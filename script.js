949988c38e5601f63d3630e87a5c31a27080ee94
nitya-vettical <138242428+nitya-vettical@users.noreply.github.com>
2024-12-19 01:40:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("surpriseButton").addEventListener("click", function(){
+    const surpriseMessage=document.getElementById("surpriseMessage");
+    surpriseMessage.textContent="guess who is taller...ME 🙋‍♀️";
+    surpriseMessage.style.display="block";
+})
